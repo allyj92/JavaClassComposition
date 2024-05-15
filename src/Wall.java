b2d350a41be1b3d311b4d610721d1b165a9ef823
eunjaelim <imeunjae361@gmail.com>
2024-05-15 18:05:00 +0900

    Initial commit

---
+++ src/Wall.java
@@ -0,0 +1,16 @@
+public class Wall {
+    private String member;
+    private String direction;
+
+    public Wall(String member) {
+        this.member = member;
+    }
+
+    public String getMember() {
+        return member;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
